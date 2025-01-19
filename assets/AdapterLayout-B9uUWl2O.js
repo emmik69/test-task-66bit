d1f062448aca5a905eff7985d7d51333d4eca04d
Emmanuil <emmanuil.litvinenko@inbox.ru>
2025-01-19 17:47:25 +0500

    Updates

--- assets/AdapterLayout-B9uUWl2O.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-BuITwzfS.js";const a=()=>t.jsx(r,{});export{a as default};
--- assets/Main-BXJ2bexR.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as e}from"./index-BuITwzfS.js";const a=()=>t.jsx(e,{to:"employees"});export{a as default};
