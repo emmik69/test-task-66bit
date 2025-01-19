b2a20a7aa2c0d747f08525b4480c556c895a6e84
Emmanuil <emmanuil.litvinenko@inbox.ru>
2025-01-19 17:43:29 +0500

    Updates

--- assets/AdapterLayout-DCt-7gmI.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as r}from"./index-DuhRgB2F.js";const a=()=>t.jsx(r,{});export{a as default};
--- assets/Main-FXOKHzbD.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as e}from"./index-DuhRgB2F.js";const a=()=>t.jsx(e,{to:"employees"});export{a as default};
