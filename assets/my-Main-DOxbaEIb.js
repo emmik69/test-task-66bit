e5c7475116cf9b48c52e8114504d9b6af3a6afd6
Emmanuil <emmanuil.litvinenko@inbox.ru>
2025-02-01 15:34:54 +0500

    Updates

---
+++ assets/my-Main-DOxbaEIb.js
@@ -0,0 +1,1 @@
+import{j as t,N as e}from"./my-index-BcxYboX2.js";const a=()=>t.jsx(e,{to:"employees"});export{a as default};
--- assets/my-Main-DhBeY18o.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as e}from"./my-index-BbOBUqOX.js";const a=()=>t.jsx(e,{to:"employees"});export{a as default};
